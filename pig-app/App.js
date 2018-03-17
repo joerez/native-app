1553f14ab85a3e0910bcbaf6f1ba7e11af6a2a9f
Joe Rezendes <rezenbiz@gmail.com>
2018-03-17 12:52:46 -0700

    Title screen

--- pig-app/App.js
+++ pig-app/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, resizeMode } from 'react-native';
 import {
   StackNavigator,
 } from 'react-navigation';
 
+
+
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+      <View style={{ flex: 1, alignItems: 'center' }}>
+      <Image
+        style={{
+          position: 'absolute',
+          height: 900,
+          width: 430
+      }}
+        source={{ uri: 'https://i.pinimg.com/originals/d3/76/e2/d376e228569f99d937d5cb69ceaab982.jpg' }}
+      />
+
         <Text style={styles.titleText}>PIG</Text>
-        <Button
-          title="PLAY NOW"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
+        <View style={styles.playBtn}>
+          <Button
+            title="PLAY NOW"
+            onPress={() => this.props.navigation.navigate('Details')}
+          />
+        </View>
       </View>
     );
   }
@@ -21,7 +35,8 @@ class HomeScreen extends React.Component {
 class DetailsScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+      <View style={{ flex: 1 }}>
         <Text>Details Screen</Text>
         <Button
           title="Go to Details... again"
@@ -62,11 +77,26 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundImage: 'url(https://i.pinimg.com/originals/d3/76/e2/d376e228569f99d937d5cb69ceaab982.jpg)',
     alignItems: 'center',
     justifyContent: 'center',
   },
   titleText: {
-    fontSize: 80
+    top: 100,
+    position: 'absolute',
+    fontSize: 80,
+    zIndex: 60
   },
+  playBtn: {
+    top: 240,
+    zIndex: 60,
+    position: 'absolute',
+    backgroundColor: '#eee',
+    height: 70,
+    width: 160,
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: 8
+  }
 });
